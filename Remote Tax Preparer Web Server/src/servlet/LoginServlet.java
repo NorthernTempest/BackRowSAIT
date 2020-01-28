e1b871024a7c9bba877aafb5d32e1f6c35686139
Jesse David Goerzen <784713@elf8it221413.ACDM.DS.SAIT.CA>
2020-01-28 08:38:47 -0700

    Minor changes to LoginServlet

--- Remote Tax Preparer Web Server/src/servlet/LoginServlet.java
+++ Remote Tax Preparer Web Server/src/servlet/LoginServlet.java
@@ -34,5 +34,4 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
-
 }
