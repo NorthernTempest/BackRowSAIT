c11e7707cc21e4320d6f3db4dad4a275f8817d79
Cesar <cesrawr@hotmail.com>
2020-02-11 15:30:47 -0700

    work on inbox

--- Remote Tax Preparer Web Server/src/manager/ParcelManager.java
+++ Remote Tax Preparer Web Server/src/manager/ParcelManager.java
@@ -1,18 +1,26 @@
 package manager;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import databaseAccess.ParcelDB;
 import domain.Parcel;
+import domain.User;
 
 /**
  * 
- * Class Description: 	Class that communicates with the ParcelDB class as a proxy
- * 						to pass information utilized in communicating with the database.
+ * Class Description: Class that communicates with the ParcelDB class as a proxy
+ * to pass information utilized in communicating with the database.
  *
- * @author Tristen Kreutz
+ * @author Tristen Kreutz, Cesar Guzman
  *
  */
-public class ParcelManager {
+public final class ParcelManager {
 
-	
+	public static ArrayList<Parcel> getByParameters(int parcelID, String sender, String receiver, Date dateSent, int taxReturnID) {
+		User userSender = UserManager.getUser(sender);
+		User userReceiver = UserManager.getUser(receiver);
+		return ParcelDB.getParcelsByParameter(parcelID, userSender, userReceiver, dateSent, taxReturnID);
+	}
+
 }
--- Remote Tax Preparer Web Server/src/manager/TaxReturnManager.java
+++ Remote Tax Preparer Web Server/src/manager/TaxReturnManager.java
@@ -2,17 +2,46 @@ package manager;
 
 import java.util.ArrayList;
 
+import databaseAccess.TaxReturnDB;
 import domain.TaxReturn;
+import domain.User;
 
 /**
  * 
  * Class Description: 	Class that communicates with the TaxReturnDB class as a proxy
  * 						to pass information utilized in communicating with the database.
  *
- * @author Tristen Kreutz
+ * @author Tristen Kreutz, Cesar Guzman
  *
  */
-public class TaxReturnManager {
-
+public final class TaxReturnManager {
 	
+	/**
+	 * Get all tax returns for a user with email as a parameter
+	 * @param email the email to get returns for
+	 * @return a list of tax returns matching the given email
+	 */
+	public static ArrayList<TaxReturn> getByEmail(String email) {
+		User user = UserManager.getUser(email);
+		
+		return TaxReturnDB.getByUser(user);
+	}
+	
+	/**
+	 * Get most recent tax return for user with email as parameter
+	 * @param email the email to get return for
+	 * @return the most recent tax return matching the given email
+	 */
+	public static int getMostRecentID(String email) {
+		ArrayList<TaxReturn> returns = TaxReturnManager.getByEmail(email);
+		
+		TaxReturn mostRecentReturn = returns.get(0);
+		for (TaxReturn taxReturn : returns) {
+			if (taxReturn.getYear() < mostRecentReturn.getYear()) {
+				mostRecentReturn = taxReturn;
+			}
+		}
+		
+		return mostRecentReturn.getTaxReturnID();
+	}
 }
